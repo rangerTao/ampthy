74c562db1625403275e8cec0f723b8b911c5ac4d
taoliang1985 <taoliang1985@638c3a2b-df72-78df-052b-fd9c1f41fcf7>
2011-06-21 13:53:56 +0000

    Finished all basic functions.

---
+++ SinaWeiboTest/src/com/weibo/pojo/adapter/CommentsToMeAdapter.java
@@ -0,0 +1,99 @@
+package com.weibo.pojo.adapter;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+
+import weibo4andriod.Comment;
+import weibo4andriod.RetweetDetails;
+import weibo4andriod.User;
+
+import com.weibo.R;
+import com.weibo.activity.IndexActivity;
+import com.weibo.utils.Constant;
+
+import android.graphics.BitmapFactory;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CommentsToMeAdapter extends BaseAdapter{
+	URL urlString;
+	@Override
+	public int getCount() {
+		return Constant.comList.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		return Constant.comList.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		return 0;
+	}
+
+	@Override
+	public View getView(int arg0, View view, ViewGroup arg2) {
+		Comment com = (Comment) getItem(arg0);
+		User user = com.getUser();
+		ViewHolder holder;
+		if(view == null){
+			LayoutInflater inflater = LayoutInflater.from(IndexActivity.appref);
+			view = inflater.inflate(R.layout.friendstimeline_adapter, null);
+			holder = new ViewHolder();
+			
+			holder.tvUserNameTextView = (TextView) view.findViewById(R.id.tvUserName);
+			holder.tvUserLocationg = (TextView) view.findViewById(R.id.tvUserLocation);
+			holder.tvUserDesc = (TextView) view.findViewById(R.id.tvUserDesc);
+			holder.tvUserDesc.setVisibility(View.GONE);
+			holder.tvUserStatus = (TextView) view.findViewById(R.id.tvStatus);
+			holder.tvTimeCreate = (TextView)view.findViewById(R.id.tvTimeCreate);
+			holder.tvSource = (TextView)view.findViewById(R.id.tvSource);
+			holder.ivUserHead = (ImageView) view.findViewById(R.id.ivUserHead);
+			
+			holder.ivStatusImage = (ImageView) view.findViewById(R.id.ivThumbail);
+			view.setTag(holder);
+		}else{
+			holder = (ViewHolder) view.getTag();
+		}
+		
+		holder.tvUserStatus.setPadding(10, 5, 0, 0);
+		holder.tvUserNameTextView.setText(user.getScreenName());
+		holder.tvUserLocationg.setText(user.getLocation());
+		
+		urlString = user.getProfileImageURL();
+		holder.ivUserHead.setImageBitmap(BitmapFactory.decodeResource(
+				IndexActivity.appref.getResources(), R.drawable.loading));
+		if (Constant.imageMap.containsKey(user.getProfileImageURL().toString()) == false) {
+			Constant.sit.pushImageTask(user.getProfileImageURL(), holder.ivUserHead);
+		} else {
+			holder.ivUserHead.setImageBitmap(Constant.imageMap.get(user.getProfileImageURL().toString() + ""));
+		}
+		holder.tvUserStatus.setText(com.getText().toString());
+		//tvSource.setText(status.getSource().toString());
+		holder.tvSource.setText(Html.fromHtml(com.getSource().toString()));
+		SimpleDateFormat sdf = new SimpleDateFormat("MM.dd HH:mm  ");
+		holder.tvTimeCreate.setText(sdf.format(com.getCreatedAt()));
+		holder.ivStatusImage.setVisibility(View.GONE);
+		
+		return view;
+	}
+	
+	static class ViewHolder{
+		TextView tvUserNameTextView;
+		TextView tvUserLocationg;
+		TextView tvUserDesc;
+		TextView tvUserStatus;
+		TextView tvTimeCreate;
+		TextView tvSource;
+		ImageView ivUserHead;
+		ImageView ivStatusImage;
+	}
+
+}
--- SinaWeiboTest/src/com/weibo/task/MailTask.java
+++ SinaWeiboTest/src/com/weibo/task/MailTask.java
@@ -8,6 +8,8 @@ import weibo4andriod.WeiboException;
 
 import com.weibo.activity.IndexActivity;
 import com.weibo.pojo.OAuthConstant;
+import com.weibo.pojo.adapter.CommentsToMeAdapter;
+import com.weibo.pojo.adapter.MailToMeAdapter;
 import com.weibo.utils.Constant;
 
 import android.os.AsyncTask;
@@ -16,13 +18,28 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class MailTask extends AsyncTask{
-
+	
+	MailToMeAdapter mtma;
+	
 	@Override
 	protected Object doInBackground(Object... arg0) {
-		getMails(Constant.mail_PageIndex);
+		if(Constant.getMsg){
+			getMails(Constant.mail_PageIndex);
+		}
 		return null;
 	}
-	
+
+	protected void onPostExecute(Object result) {
+
+		mtma = new MailToMeAdapter();
+		if (IndexActivity.lvHomeTimeLine.getAdapter() != null ){
+			IndexActivity.lvHomeTimeLine.removeAllViewsInLayout();
+			IndexActivity.lvHomeTimeLine.setAdapter(mtma);
+		}else{
+			mtma.notifyDataSetChanged();
+		}
+		super.onPostExecute(result);
+	}
 	private void getMails(int page) {
 		Weibo4sina weibo = OAuthConstant.getInstance().getWeibo();
 		try {
